353dbaf1f7280c94b3ccac472abd07ce0829b0dd
aryansingh120 <aryanhp940@gmail.com>
2025-12-15 16:26:56 +0530

    json added

--- src/app/page.js
+++ src/app/page.js
@@ -22,6 +22,7 @@ export default async function Home() {
     const h = await headers(); // ✅ MUST await in Next 16
 
   const domain = h.get("host") || "localhost";
+  console.log("Current Domain:", domain);
 
   const pageData =
     SITE_DATA.find((item) => item.domain === domain) ||
