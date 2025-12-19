a70cd681c24e515355d9adb1e9f1efc1183aa454
aryansingh120 <aryanhp940@gmail.com>
2025-12-19 12:06:48 +0530

    navbar remove

---
+++ src/lib/googleVerification.js
@@ -0,0 +1,8 @@
+export const GOOGLE_VERIFICATION = {
+  "flatsforsaleinhisar.com": "3vF9ESnurvBS1s5ksxMnUrl5UI8VYUln0Kg_k1JiXAk",
+  "faridabad.projectwallah.live": "GSC_CODE_FARIDABAD",
+  "gurgaon.projectwallah.live": "GSC_CODE_GURGAON",
+
+  // fallback
+  "localhost": "GSC_CODE_DEFAULT",
+};
