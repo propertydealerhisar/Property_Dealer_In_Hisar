ebf7d083e26829495a241dfe07c867c0c2122cd0
aryansingh120 <aryanhp940@gmail.com>
2025-12-19 12:15:06 +0530

    navbar remove

--- src/lib/googleVerification.js
+++ src/lib/googleVerification.js
@@ -1,8 +1,8 @@
 export const GOOGLE_VERIFICATION = {
   "www.flatsforsaleinhisar.com": "3vF9ESnurvBS1s5ksxMnUrl5UI8VYUln0Kg_k1JiXAk",
   "www.flatsforrentinhisar.com": "I6VatZOXsoEtR7wJXeVkkL3WCzPPl4wj02ytBiFh4NA",
-  "faridabad.projectwallah.live": "GSC_CODE_FARIDABAD",
-  "faridabad.projectwallah.live": "GSC_CODE_FARIDABAD",
+  "www.houseforsaleinhisar.com": "MSIZ_p9iTeHQ4NjyZMhXI-Ju0XW8gmENrJ9zdudhG14",
+  "www.houseforrentinhisar.com": "GSC_CODE_FARIDABAD",
   "faridabad.projectwallah.live": "GSC_CODE_FARIDABAD",
   "faridabad.projectwallah.live": "GSC_CODE_FARIDABAD",
   "faridabad.projectwallah.live": "GSC_CODE_FARIDABAD",
