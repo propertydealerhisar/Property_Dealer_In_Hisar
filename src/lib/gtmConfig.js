7ed6337e6e2d03b80f78bc8789b92e8205119f22
aryansingh120 <aryanhp940@gmail.com>
2025-12-19 13:40:08 +0530

    google tag manager added

--- src/lib/gtmConfig.js
+++ src/lib/gtmConfig.js
@@ -3,17 +3,13 @@ export const GTM_IDS = {
   "www.flatsforrentinhisar.com": "GTM-MD7W3G3K",
   "www.houseforsaleinhisar.com": "GTM-PP7Z5FNP",
   "www.houseforrentinhisar.com": "GTM-N9FW6DJH",
-  "www.shopforsaleinhisar.com": "GTM-NL4P6JP3",
+  "www.shopforsaleinhisar.com": "GTM-WDRGJWQK",
   "www.commercialpropertyforsaleinhisar.com": "GTM-NL4P6JP3",
   "www.shopforrentinhisar.com": "GTM-NL4P6JP3",
   "www.plotforsaleinhisar.com": "GTM-NL4P6JP3",
-  "www.": "GTM-NL4P6JP3",
-  "www.": "GTM-NL4P6JP3",
-  "www.": "GTM-NL4P6JP3",
-  "www.": "GTM-NL4P6JP3",
-  "www.": "GTM-NL4P6JP3",
-  "www.": "GTM-NL4P6JP3",
-  "localhost": "GTM-NL4P6JP3",
+  "www.agriculturelandforsaleinhisar.com": "GTM-NL4P6JP3",
+  "www.toletserviceinhisar.com": "GTM-NL4P6JP3",
+  
 
   // fallback (localhost / default)
   "localhost": "GTM-NL4P6JP3",
