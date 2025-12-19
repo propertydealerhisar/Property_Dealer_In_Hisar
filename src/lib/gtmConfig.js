fefd8cce8de24ccfcf137f96f67d5b540d7a6278
aryansingh120 <aryanhp940@gmail.com>
2025-12-19 13:20:01 +0530

    google tag manager added

--- src/lib/gtmConfig.js
+++ src/lib/gtmConfig.js
@@ -1,6 +1,6 @@
 export const GTM_IDS = {
   "www.flatsforsaleinhisar.com": "GTM-NL4P6JP3",
-  "faridabad.projectwallah.live": "GTM-BBBBBBB",
+  "flatsforrentinhisar.com": "GTM-MD7W3G3K",
   "gurgaon.projectwallah.live": "GTM-CCCCCCC",
 
   // fallback (localhost / default)
