1bc73f7663429e95c76e2919f742e6857493123e
aryansingh120 <aryanhp940@gmail.com>
2025-12-23 13:12:30 +0530

    all update

--- src/lib/main-domain/gaTargetDomains.js
+++ src/lib/main-domain/gaTargetDomains.js
@@ -1,6 +1,11 @@
 export const GA_TARGET_DOMAINS = {
   "www.agriculturelandforsaleinhisar.com": "G-HRXNSXE7GL",
   "www.flatsforsaleinhisar.com": "G-38314VXR2X",
+  "www.flatsforrentinhisar.com": "G-RW9XBDPTMK",
+
+
+
+
 
   // fallback (localhost / default)
   "localhost": "G-ABCD1234",
