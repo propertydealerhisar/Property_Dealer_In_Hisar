ef5ae07f1deb4a3ba3aa071087ffb4fb6a5caa6e
aryansingh120 <aryanhp940@gmail.com>
2026-01-09 15:36:11 +0530

    code update

--- src/app/main-domain/layout.js
+++ src/app/main-domain/layout.js
@@ -63,8 +63,8 @@ export async function generateMetadata() {
 
 export default async function RootLayout({ children }) {
   const h = await headers();
-  // const domain = h.get("host") || "localhost";
-  const domain = "www.plotingurgaon.in"
+  const domain = h.get("host") || "localhost";
+  // const domain = "www.plotingurgaon.in"
      const pageData = loadPageData(domain);
      
   if (!pageData) return notFound();
