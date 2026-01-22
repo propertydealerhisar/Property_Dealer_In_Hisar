c14bec44477e9d3020d4f49b68d8253e3fcd2d83
aryansingh120 <aryanhp940@gmail.com>
2026-01-22 13:23:20 +0530

    add main domain config

--- src/lib/main-domain/gtmConfig.js
+++ src/lib/main-domain/gtmConfig.js
@@ -15,7 +15,7 @@ export const GTM_IDS = {
   "www.worldstreetfaridabad.com": "",
   "www.affordablehouseinfaridabad.com": "",
   "www.3bhkflatforsaleinfaridabad.com": "GTM-KZT7TMZH",
-  "www.shopforsaleinfaridabad.com": "",
+  "www.shopforsaleinfaridabad.com": "GTM-T2VVCXVR",
   "www.flatforrentinfaridabad.com": "",
   "www.plotforsaleinfaridabad.com": "",
   "www.agriculturelandforsaleinfaridabad.com": "",
